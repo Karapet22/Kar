5368741f63a423cddeed7996af9918f3b49f349e
Karapet22 <kar.satxa.03@mail.ru>
2024-10-15 13:21:30 +0400

    Add files via upload

---
+++ bazmapatik.py
@@ -0,0 +1,14 @@
+# from math import gcd
+# a = int(input('enter a number: '))
+# b = int(input('enter a number: '))
+# x = gcd(a,b)
+# print(x)
+def x(a,b):
+    while b:
+        b,a % b
+        if b == 0:
+            return abs(a)  
+a = int(input('enter anumber : '))
+b = int(input('enter a number : '))
+result = x(a, b)
+print(result)
